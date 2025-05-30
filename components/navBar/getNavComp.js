7562eacf0236484c106062dcab068d46190de68f
rolandawoda <rolandawoda@gmail.com>
2025-05-30 02:20:36 +0100

    Finished header functionality

---
+++ components/navBar/getNavComp.js
@@ -0,0 +1,31 @@
+// function to be run for the nav component when the index.html document is completely done loading
+function rehidrateNavbarEvents() {
+  if (document.querySelector("#toggle-btn")) {
+    document.querySelector("#toggle-btn").addEventListener("click", () => {
+      let navBar = document.querySelector(".navbar");
+      navBar.classList.toggle("navbar-display");
+    });
+  }
+}
+
+function getNav() {
+  fetch("../../components/navBar/nav.html")
+    .then((response) => response.text())
+    .then((html) => {
+      let temp = document.createElement("div");
+      let header = document.querySelector("header");
+      temp.innerHTML = html;
+      if (header.firstChild) {
+        header.insertBefore(temp.firstChild, header.firstChild);
+      } else {
+        header.append(temp.firstChild);
+      }
+      //rehydrate navbar events
+      rehidrateNavbarEvents();
+    })
+    .catch((err) =>
+      console.log("error occured when fetching nav component", err)
+    );
+}
+
+getNav();
